fef4eb60b450d84f512f1408e949b083b33870b6
leavedtrait <leavedtrait@gmail.com>
2024-09-25 23:30:28 +0300

    first commit

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,118 @@
+<script>
+	let isMenuOpen = false;
+</script>
+
+<div class="drawer">
+	<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col">
+		<div class="navbar fixed top-0 bg-base-200/30 p-3 backdrop-blur-md">
+			<!-- Mobile Menu Button (Shown on small devices) -->
+			<div class="flex-none lg:hidden">
+				<label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						class="inline-block h-6 w-6 stroke-current"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h16"
+						></path>
+					</svg>
+				</label>
+			</div>
+
+			<!-- Navbar brand -->
+			<div class="navbar-start flex-1">
+				<a class="btn btn-ghost text-xl font-extrabold" href="/">LeavedTrait</a>
+			</div>
+
+			<!-- Centered Menu (Hidden on small devices, shown on large devices) -->
+			<div class="navbar-center hidden flex-1 lg:flex">
+				<ul class="menu menu-horizontal space-x-3 text-xl font-semibold">
+					<li>
+						<a href="/projects">projects</a>
+					</li>
+					<li>
+						<a href="/cv">cv</a>
+					</li>
+					<li>
+						<a href="/blog">blog</a>
+					</li>
+				</ul>
+			</div>
+
+			<!-- External Links -->
+			<ul class="menu menu-horizontal flex-none">
+				<li>
+					<a href="https://github.com/leavedtrait">
+						<svg xmlns="http://www.w3.org/2000/svg" width="30" height="28" viewBox="0 0 30 30">
+							<path
+								d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 
+                    c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 
+                    c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 
+                    c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 
+                    c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481 
+                    C13.477,9.174,14.461,9,15.495,9c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 
+                    c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 
+                    c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"
+							></path>
+						</svg>
+					</a>
+				</li>
+				<li>
+					<a href="https://www.linkedin.com/in/leaved-trait-60536b2b2/">
+						<svg xmlns="http://www.w3.org/2000/svg" width="30" height="28" viewBox="0 0 50 50">
+							<path
+								d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z 
+                    M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 
+                    c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 
+                    c3.97,0,7.19,2.73,7.19,8.26V39z"
+							></path>
+						</svg>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="drawer-side">
+		<label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
+		<ul class="menu menu-vertical min-h-full w-80 bg-base-200 p-4">
+			<!-- Sidebar content here -->
+
+			<!-- Navbar brand -->
+			<div class="flex-1 space-x-20">
+				<a class="btn btn-ghost text-xl font-extrabold" href="/">LeavedTrait</a>
+				<label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						class="inline-block h-6 w-6 stroke-current"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M6 18L18 6M6 6l12 12"
+						></path>
+					</svg>
+				</label>
+			</div>
+
+			<li>
+				<a href="/projects">projects</a>
+			</li>
+			<li>
+				<a href="/cv">cv</a>
+			</li>
+			<li>
+				<a href="/blog">blog</a>
+			</li>
+		</ul>
+	</div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import NavBar from '$lib/components/NavBar.svelte';
+	import '../app.css';
+</script>
+<main class="py-10">
+	<NavBar/>
+	<div class="">
+		<slot></slot>
+
+	</div>
+	
+</main>
+
