5c22644accdec507665c71e73045b9034aea85f0
leavedtrait <leavedtrait@gmail.com>
2024-09-26 15:21:18 +0300

    add seo improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 
 <svelte:head>
 	<title>Johnson Macharia</title>
+  <meta name="description" content="View my portfolio">
 </svelte:head>
 <div class="h-full min-w-full py-12">
 	<div class="flex flex-col items-center">
