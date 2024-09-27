e8e86a3c29f9c5f81cbe527d4b113bea4f64813d
leavedtrait <leavedtrait@gmail.com>
2024-09-28 02:38:32 +0300

    fixed reponsiveness on large screens

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,27 +2,29 @@
 	import Seo from '$lib/components/Seo.svelte';
 	import ProjectCard from '$lib/components/ProjectCard.svelte';
 	import type { projectCardProps, seoConfigType } from '$lib/types';
+
 	const seoConfig: seoConfigType = {
 		title: 'Johnson Macharia',
 		description: 'list of my projects'
 	};
+
 	let projectList: projectCardProps[] = [
+		{
+			title: 'Promptopia',
+			imageUrl:
+				'https://res.cloudinary.com/dupkisnya/image/upload/f_webp/Screenshot_from_2024-09-28_00-03-07_g4syg9.jpg',
+			description: 'Introducing Promptopia, a website built for sharing AI prompts.',
+			githubUrl: 'https://github.com/leavedtrait/share-prompts',
+			liveUrl: 'https://share-prompts-dusky.vercel.app/'
+		},
 		{
 			imageUrl:
 				'https://res.cloudinary.com/dupkisnya/image/upload/f_webp/Screenshot_from_2024-09-28_00-40-16_fthugj.jpg',
 			title: 'My portfolio',
 			description:
-				'My portfolio/blog, built with Sveltekit and Tailwind.The blog is powered  by MDsveX.',
+				'My portfolio/blog, built with SvelteKit and Tailwind. The blog is powered by MDsveX.',
 			githubUrl: 'https://github.com/leavedtrait/portfolio',
 			liveUrl: 'https://leavedtrait.vercel.app'
-		},
-		{
-			title: 'Promptopia',
-			imageUrl:
-				'https://res.cloudinary.com/dupkisnya/image/upload/f_webp/Screenshot_from_2024-09-28_00-03-07_g4syg9.jpg',
-			description: 'Introducing Promptopia ,a website built for sharing Ai prompts.',
-			githubUrl: 'https://github.com/leavedtrait/share-prompts',
-			liveUrl: 'https://share-prompts-dusky.vercel.app/'
 		}
 	];
 </script>
@@ -30,9 +32,17 @@
 <Seo {seoConfig} />
 
 <div class="flex flex-col items-center scroll-smooth">
-	<div
-		class="grid justify-center gap-6 space-x-4 p-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-8 xl:grid-cols-4"
-	>
+	<!-- Grid layout for small and medium devices -->
+	<div class="lg:hidden p-4 grid justify-center gap-6 sm:grid-cols-1 md:grid-cols-2">
+		{#each projectList as props}
+			<div class="-z-[1] mx-auto">
+				<ProjectCard {props} />
+			</div>
+		{/each}
+	</div>
+
+	<!-- Flexbox layout for large devices and above -->
+	<div class="hidden lg:grid lg:grid-cols-2 xl:grid-cols-3 gap-10 p-6 m-auto">
 		{#each projectList as props}
 			<div class="-z-[1] mx-auto">
 				<ProjectCard {props} />
