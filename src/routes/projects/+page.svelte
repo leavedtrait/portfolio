c2013b5f4dbf37049d1edbe76286399fe7fb9300
leavedtrait <leavedtrait@gmail.com>
2024-09-28 15:11:59 +0300

    modified:   src/routes/projects/+page.svelte

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -27,7 +27,46 @@
 			githubUrl: 'https://github.com/leavedtrait/portfolio',
 			liveUrl: 'https://leavedtrait.vercel.app',
 			tags: ['SvelteKit', 'Tailwind', 'MDsveX']
+		},
+		{
+			title: 'Promptopia',
+			imageUrl:
+				'https://res.cloudinary.com/dupkisnya/image/upload/f_webp/Screenshot_from_2024-09-28_00-03-07_g4syg9.jpg',
+			description: 'Introducing Promptopia, a website built for sharing AI prompts.',
+			githubUrl: 'https://github.com/leavedtrait/share-prompts',
+			liveUrl: 'https://share-prompts-dusky.vercel.app/',
+			tags: ['Nextjs', 'MongoDB', 'Next-auth']
+		},
+		{
+			imageUrl:
+				'https://res.cloudinary.com/dupkisnya/image/upload/f_webp/Screenshot_from_2024-09-28_00-40-16_fthugj.jpg',
+			title: 'My portfolio',
+			description:
+				'My portfolio/blog, built with SvelteKit and Tailwind. The blog is powered by MDsveX.',
+			githubUrl: 'https://github.com/leavedtrait/portfolio',
+			liveUrl: 'https://leavedtrait.vercel.app',
+			tags: ['SvelteKit', 'Tailwind', 'MDsveX']
+		},
+		{
+			title: 'Promptopia',
+			imageUrl:
+				'https://res.cloudinary.com/dupkisnya/image/upload/f_webp/Screenshot_from_2024-09-28_00-03-07_g4syg9.jpg',
+			description: 'Introducing Promptopia, a website built for sharing AI prompts.',
+			githubUrl: 'https://github.com/leavedtrait/share-prompts',
+			liveUrl: 'https://share-prompts-dusky.vercel.app/',
+			tags: ['Nextjs', 'MongoDB', 'Next-auth']
+		},
+		{
+			imageUrl:
+				'https://res.cloudinary.com/dupkisnya/image/upload/f_webp/Screenshot_from_2024-09-28_00-40-16_fthugj.jpg',
+			title: 'My portfolio',
+			description:
+				'My portfolio/blog, built with SvelteKit and Tailwind. The blog is powered by MDsveX.',
+			githubUrl: 'https://github.com/leavedtrait/portfolio',
+			liveUrl: 'https://leavedtrait.vercel.app',
+			tags: ['SvelteKit', 'Tailwind', 'MDsveX']
 		}
+
 	];
 </script>
 
