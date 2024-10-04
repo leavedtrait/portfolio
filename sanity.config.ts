a009f8d403240249e827c94e7c007e32d8cc73b5
leavedtrait <leavedtrait@gmail.com>
2024-10-04 03:36:40 +0300

    adding sanity cms

---
+++ sanity.config.ts
@@ -0,0 +1,19 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './src/lib/schemaTypes'
+
+export default defineConfig({
+  basePath: "/studio",
+  name: 'default',
+  title: 'Sveltekit Blog',
+
+  projectId: 'tjedknvi',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
