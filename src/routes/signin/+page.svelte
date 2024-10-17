511b34c289b067b743733274885ac9c1203d2312
leavedtrait <leavedtrait@gmail.com>
2024-10-17 09:32:16 +0300

    added sveltekit auth

---
+++ src/routes/signin/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+   import { SignIn } from "@auth/sveltekit/components"
+</script>
+ 
+<div>
+  <img src="/img/logo.svg" alt="Company Logo" />
+  <SignIn provider="github" signInPage="signin" />
+</div> 
